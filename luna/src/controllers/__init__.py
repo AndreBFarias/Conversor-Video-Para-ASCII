11384ddd4b5f89a00d3a88ceec10b18ee843f262
AndreBFarias <andre.dsbf@gmail.com>
2025-12-31 14:41:35 -0300

    Update

--- luna/src/controllers/__init__.py
+++
@@ -1,5 +0,0 @@
-from .audio_controller import AudioController
-from .threading_controller import ThreadingController
-from .ui_controller import UIController
-
-__all__ = ["UIController", "ThreadingController", "AudioController"]
